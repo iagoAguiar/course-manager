216873e84462c561d72b44eecac25b6fe8bc19b4
Iago Duarte de Aguiar <iagoaguiar202@gmail.com>
2021-08-26 21:36:58 -0300

    Ativado rotas para acessar componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
+import { CourseInfoComponent } from './courses/courses.info.components';
+import { Error404Component } from './error-404/error-404.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/courses-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent 
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent 
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
